676f57f8e2668f36abb20fbc82432c357a9f5cb9
Eugen Scutaru <evgensc5@gmail.com>
2026-09-11 18:42:22 +0300

    Initial commit

---
+++ src/main/java/pixel/academy/resr_crud_app/ResrCrudAppApplication.java
@@ -0,0 +1,13 @@
+package pixel.academy.resr_crud_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResrCrudAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResrCrudAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pixel/academy/resr_crud_app/ResrCrudAppApplicationTests.java
@@ -0,0 +1,13 @@
+package pixel.academy.resr_crud_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResrCrudAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
