853cd0c2a897e14932f7b68139ec807af28ebf10
Eugen Scutaru <evgensc5@gmail.com>
2026-09-12 10:24:51 +0300

    Implement new class NotFound and ErrorResponse

---
+++ src/main/java/pixel/academy/rest_crud_app/rest/StudentErrorResponse.java
@@ -0,0 +1,43 @@
+package pixel.academy.rest_crud_app.rest;
+
+public class StudentErrorResponse {
+
+    private int status;
+
+    private String message;
+
+    private long timeStamp;
+
+    public StudentErrorResponse(int status, String message, long timeStamp) {
+        this.status = status;
+        this.message = message;
+        this.timeStamp =timeStamp;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public long getTimeStamp() {
+        return timeStamp;
+    }
+
+    public void setTimeStamp(long timeStamp) {
+        this.timeStamp = timeStamp;
+    }
+    public StudentErrorResponse() {
+
+    }
+}
